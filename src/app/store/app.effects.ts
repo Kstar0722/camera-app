f2d503805274460a8318b57965d8ddf8edf70b11
Erik <erikrorstrom612@gmail.com>
2019-10-15 04:36:58 -0400

    define effect

--- src/app/store/app.effects.ts
+++ src/app/store/app.effects.ts
@@ -4,81 +4,25 @@ import { of } from 'rxjs';
 import { switchMap, mapTo, concatMapTo } from 'rxjs/operators';
 
 import {
-  EAppActions,
-  GetDomainName,
-  GetDomainData,
-  GetDomainDataSuccess,
-  GetActualData,
-  SetActualData,
-  GetActualDataSuccess,
-  UpdateGroupColor,
-  UpdateGroupColorSuccess,
-  UpdateGroupOrder,
-  UpdateGroupOrderSuccess,
-  GetChartData,
-  GetChartDataSuccess,
-  SetChartData,
-  UpdateShowHideTeam,
-  UpdateShowHideTeamSuccess
+  ELocationActions,
+  GetLocations,
+  GetLocationsSuccess
 } from './app.actions';
 
-import { ChartDataService } from 'app/service/chart-data.service';
-import { ParsingService } from 'app/service/parsing.service';
-import { IActualData, IChartData } from './app.state';
+import { ILocation } from './app.state';
 
 @Injectable()
 export class AppEffects {
   constructor(
-    private _action$: Actions,
-    private _chartDataService: ChartDataService,
-    private _parsingService: ParsingService
+    private _action$: Actions
   ) { }
 
   @Effect()
-  getDomainData$ = this._action$.pipe(
-    ofType<GetDomainName>(EAppActions.GetDomainName),
+  getLocations$ = this._action$.pipe(
+    ofType<GetLocations>(ELocationActions.GetLocations),
     switchMap(action => this._chartDataService.getTxtData(action.payload)),
-    switchMap((txtData: [string, string]) => [
-      new GetDomainData(txtData),
-      new GetDomainDataSuccess()
+    switchMap((locations: Array<ILocation>) => [
+      new GetLocationsSuccess(locations)
     ])
-  );
-
-  @Effect()
-  getActualData$ = this._action$.pipe(
-    ofType<GetActualData>(EAppActions.GetActualData),
-    switchMap(action => this._parsingService.parsingHumansTxtAndTeamsTxt(action.payload.humansTxt.new, action.payload.teamsTxt.new)),
-    switchMap((actualData: IActualData) => [
-      new SetActualData(actualData),
-      new GetActualDataSuccess()
-    ])
-  );
-
-  @Effect()
-  getChartData$ = this._action$.pipe(
-    ofType<GetChartData>(EAppActions.GetChartData),
-    switchMap(action => this._chartDataService.getChartdata(action.payload)),
-    switchMap((chartData: IChartData) => [
-      new SetChartData(chartData),
-      new GetChartDataSuccess()
-    ])
-  );
-
-  @Effect()
-  updateGroupColor$ = this._action$.pipe(
-    ofType<UpdateGroupColor>(EAppActions.UpdateGroupColor),
-    switchMap(action => [new UpdateGroupColorSuccess()])
-  );
-
-  @Effect()
-  updateGroupOrder$ = this._action$.pipe(
-    ofType<UpdateGroupOrder>(EAppActions.UpdateGroupOrder),
-    switchMap(action => [new UpdateGroupOrderSuccess()])
-  );
-
-  @Effect()
-  updateShowHideTeam$ = this._action$.pipe(
-    ofType<UpdateShowHideTeam>(EAppActions.UpdateShowHideTeam),
-    switchMap(action => [new UpdateShowHideTeamSuccess()])
   );
 }
